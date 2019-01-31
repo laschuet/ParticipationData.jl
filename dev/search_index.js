343df9a04568aa51a6cf14399fda535f34b76d81
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-31 09:18:13 +0000

    build based on cbe21aa

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#ParticipationData.jl-1",
+    "page": "Home",
+    "title": "ParticipationData.jl",
+    "category": "section",
+    "text": ""
+},
+
+]}
